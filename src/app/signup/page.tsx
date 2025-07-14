065f1e3d91f90f751c4c97d01884c5847f6a9203
uma-mahesh-mekala <maheshuma154@gmail.com>
2025-07-14 10:09:17 +0530

    feat: update signup components

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,5 +1,5 @@
 import AuthForm from "@/components/AuthForm";
 
-export default function LoginPage() {
-  return <AuthForm title="Sign Up" buttonText="Sign Up" />;
+export default function SignUpPage() {
+  return <AuthForm mode="signup" />;
 }
